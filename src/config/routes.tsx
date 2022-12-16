8e9934df72bc189fbf60ca026dde4fd6798069d8
czf <505487452@qq.com>
2022-12-16 10:34:21 +0800

    创建欢迎界面路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,21 @@
 import {Foo} from '../views/Foo';
 import {Bar} from '../views/Bar';
+import {Welcome} from '../views/Welcome';
+import {RouteRecordRaw} from 'vue-router';
+import {First} from '../views/First';
+import {Second} from '../views/Second';
+import {Third} from '../views/Third';
+import {Forth} from '../views/Forth';
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/Bar', component: Bar },
+  {path: '/welcome',
+  component: Welcome,
+  children: [
+    {path:'1', component: First},
+    {path:'2', component: Second},
+    {path:'3', component: Third},
+    {path:'4', component: Forth},
+  ]}
 ]
