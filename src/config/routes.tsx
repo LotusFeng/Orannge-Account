97d6883ce62af3d38f81174e58888166acc50775
czf <505487452@qq.com>
2022-12-19 15:16:20 +0800

    修改默认路由，禁止缩放

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,5 +1,3 @@
-import {Foo} from '../views/Foo';
-import {Bar} from '../views/Bar';
 import {Welcome} from '../views/Welcome';
 import {RouteRecordRaw} from 'vue-router';
 import {First} from '../components/welcome/First';
@@ -8,11 +6,10 @@ import {Third} from '../components/welcome/Third';
 import {Forth} from '../components/welcome/Forth';
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
-  { path: '/Bar', component: Bar },
   {path: '/welcome',
   component: Welcome,
   children: [
+    {path:'/', redirect:'/welcome/1'},
     {path:'1', component: First},
     {path:'2', component: Second},
     {path:'3', component: Third},
