abe1b7226db37003861bbd624888732312567b95
czf <505487452@qq.com>
2022-12-20 16:18:01 +0800

    欢迎界面添加动画

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
+import { routes } from './config/routes';
 import { createApp } from 'vue'
-import {App} from './App'
-import {createRouter} from 'vue-router';
-import {routes} from './config/routes';
-import {history} from './shared/history';
+import { App } from './App'
+import { createRouter } from 'vue-router'
+import { history } from './shared/history';
 
-
-const router = createRouter({history, routes,})
+const router = createRouter({ history, routes })
 
 const app = createApp(App)
-  app.use(router)
-  app.mount('#app')
+app.use(router)
+app.mount('#app')
