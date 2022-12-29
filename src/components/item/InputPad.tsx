f484f82ae87e9bf3f3ac9b66d53b45157060332e
czf <505487452@qq.com>
2022-12-29 16:39:30 +0800

    引入vant ui 实现日期选择

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -2,6 +2,7 @@ import {defineComponent, PropType, ref} from 'vue';
 import s from './InputPad.module.scss';
 import {Icon} from '../../shared/Icon';
 import {time} from '../../shared/time';
+import { DatetimePicker, Popup } from 'vant';
 
 export const InputPad = defineComponent({
   props: {
@@ -10,8 +11,8 @@ export const InputPad = defineComponent({
     }
   },
   setup: (props, context) => {
-    const refDate = ref<Date>()
     const now = new Date()
+    const refDate = ref<Date>(now)
     const buttons = [
       { text: '1', onClick: () => { } },
       { text: '2', onClick: () => { } },
@@ -30,12 +31,20 @@ export const InputPad = defineComponent({
       { text: '删', onClick: () => { } },
       { text: '提交', onClick: () => { } },
     ]
+    const refDatePickerVisible = ref(false)
+    const showDatePicker = () => refDatePickerVisible.value = true
+    const hideDatePicker = () => refDatePickerVisible.value = false
+    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
           <Icon name="date" class={s.icon}/>
           <span>
-            <input type="date" value={time(now).format()}/>
+            <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
+              <DatetimePicker value={refDate.value} type="date" title="选择年月日"
+                              onConfirm={setDate} onCancel={hideDatePicker}/>
+            </Popup>
           </span>
         </span>
         <span class={s.amount}>199.12</span>
