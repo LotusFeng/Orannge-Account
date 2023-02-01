153316b29bee646f14c0b1993ca61fe62c11753e
czf <505487452@qq.com>
2023-02-01 14:59:32 +0800

    封装Tags组件

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,15 +1,52 @@
 import {defineComponent, PropType} from 'vue';
 import s from './Tags.module.scss';
+import {Icon} from '../../shared/Icon';
+import {Button} from '../../shared/Button';
+import { useTags } from '../../shared/useTags';
+import {http} from '../../shared/Http';
 
 export const Tags = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>
+    kind: {
+      type: String as PropType<string>,
+      required: true
     }
   },
   setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>$2</div>
-    );
+    const { tags: expensesTags, hasMore, fetchTags } = useTags((page) => {
+      return http.get<Resources<Tag>>('/tags', {
+        kind: props.kind,
+        page: page + 1,
+        _mock: 'tagIndex'
+      })
+    })
+    return () => <>
+      <div class={s.tags_wrapper}>
+        <div class={s.tag}>
+          <div class={s.sign}>
+            <Icon name="add" class={s.createTag}/>
+          </div>
+          <div class={s.name}>
+            新增
+          </div>
+        </div>
+        {expensesTags.value.map(tag =>
+          <div class={[s.tag, s.selected]}>
+            <div class={s.sign}>
+              {tag.sign}
+            </div>
+            <div class={s.name}>
+              {tag.name}
+            </div>
+          </div>
+        )}
+      </div>
+      <div class={s.more}>
+        {hasMore.value ?
+          <Button class={s.loadMore} onClick={fetchTags}>加载更多</Button> :
+          <span class={s.noMore}>没有更多</span>
+        }
+      </div>
+    </>;
   }
 });
