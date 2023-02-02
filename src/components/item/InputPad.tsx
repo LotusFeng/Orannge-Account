b91188571185b313fdee927663c35e743158d07d
czf <505487452@qq.com>
2023-02-02 10:53:29 +0800

    可以新增记账

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,75 +1,84 @@
-import { defineComponent, PropType, ref } from 'vue';
-import { Icon } from '../../shared/Icon';
-import { Time } from '../../shared/time';
+import {defineComponent, PropType, ref} from 'vue';
+import {Icon} from '../../shared/Icon';
+import {Time} from '../../shared/time';
 import s from './InputPad.module.scss';
-import { DatetimePicker, NumberKeyboard, Popup } from 'vant';
+import {DatetimePicker, NumberKeyboard, Popup} from 'vant';
+
 export const InputPad = defineComponent({
   props: {
     happenAt: String,
-    amount: Number
+    amount: Number,
+    onSubmit: {
+      type: Function as PropType<() => void>
+    }
   },
   setup: (props, context) => {
-    const now = new Date()
-    const refDate = ref<Date>(now)
+    const now = new Date();
+    const refDate = ref<Date>(now);
     const appendText = (n: number | string) => {
-      const nString = n.toString()
-      const dotIndex = refAmount.value.indexOf('.')
+      const nString = n.toString();
+      const dotIndex = refAmount.value.indexOf('.');
       if (refAmount.value.length >= 13) {
-        return
+        return;
       }
       if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
-        return
+        return;
       }
       if (nString === '.') {
         if (dotIndex >= 0) { // 已经有小数点了
-          return
+          return;
         }
       } else if (nString === '0') {
         if (dotIndex === -1) { // 没有小数点
           if (refAmount.value === '0') { // 没小数点，但是有0
-            return
+            return;
           }
         }
       } else {
         if (refAmount.value === '0') {
-          refAmount.value = ''
+          refAmount.value = '';
         }
       }
-      refAmount.value += n.toString()
-    }
+      refAmount.value += n.toString();
+    };
     const buttons = [
-      { text: '1', onClick: () => { appendText(1) } },
-      { text: '2', onClick: () => { appendText(2) } },
-      { text: '3', onClick: () => { appendText(3) } },
-      { text: '4', onClick: () => { appendText(4) } },
-      { text: '5', onClick: () => { appendText(5) } },
-      { text: '6', onClick: () => { appendText(6) } },
-      { text: '7', onClick: () => { appendText(7) } },
-      { text: '8', onClick: () => { appendText(8) } },
-      { text: '9', onClick: () => { appendText(9) } },
-      { text: '.', onClick: () => { appendText('.') } },
-      { text: '0', onClick: () => { appendText(0) } },
-      { text: '清空', onClick: () => { refAmount.value = '0' } },
-      { text: '提交', onClick: () => context.emit('update:amount',
-          parseFloat(refAmount.value) * 100) },
-    ]
-    const refDatePickerVisible = ref(false)
-    const showDatePicker = () => refDatePickerVisible.value = true
-    const hideDatePicker = () => refDatePickerVisible.value = false
+      {text: '1', onClick: () => { appendText(1); }},
+      {text: '2', onClick: () => { appendText(2); }},
+      {text: '3', onClick: () => { appendText(3); }},
+      {text: '4', onClick: () => { appendText(4); }},
+      {text: '5', onClick: () => { appendText(5); }},
+      {text: '6', onClick: () => { appendText(6); }},
+      {text: '7', onClick: () => { appendText(7); }},
+      {text: '8', onClick: () => { appendText(8); }},
+      {text: '9', onClick: () => { appendText(9); }},
+      {text: '.', onClick: () => { appendText('.'); }},
+      {text: '0', onClick: () => { appendText(0); }},
+      {text: '清空', onClick: () => { refAmount.value = '0'; }},
+      {
+        text: '提交', onClick: () => {
+          context.emit('update:amount',
+            parseFloat(refAmount.value) * 100);
+          props.onSumbmit?.();
+        }
+      },
+    ];
+    const refDatePickerVisible = ref(false);
+    const showDatePicker = () => refDatePickerVisible.value = true;
+    const hideDatePicker = () => refDatePickerVisible.value = false;
     const setDate = (date: Date) => {
       context.emit('update:happenAt', date.toISOString());
-      hideDatePicker()
-    }
-    const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0')
+      hideDatePicker();
+    };
+    const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0');
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
-          <Icon name="date" class={s.icon} />
+          <Icon name="date" class={s.icon}/>
           <span>
             <span onClick={showDatePicker}>{new Time(props.happenAt).format()}</span>
-            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
+            <Popup position="bottom" v-model:show={refDatePickerVisible.value}>
               <DatetimePicker value={props.happenAt} type="date" title="选择年月日"
-                onConfirm={setDate} onCancel={hideDatePicker}
+                              onConfirm={setDate} onCancel={hideDatePicker}
               />
             </Popup>
           </span>
@@ -81,6 +90,6 @@ export const InputPad = defineComponent({
           <button onClick={button.onClick}>{button.text}</button>
         )}
       </div>
-    </>
+    </>;
   }
-})
+});
