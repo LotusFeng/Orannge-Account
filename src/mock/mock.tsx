d23a4c44c75ac931f03d5277f701dd8cab146abe
czf <505487452@qq.com>
2023-02-06 21:56:19 +0800

    mock tagEdit

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -20,6 +20,17 @@ export const mockItemCreate: Mock = config => {
     }
   }]
 }
+export const mockTagEdit: Mock = config => {
+  const createTag = (attrs?: any) =>
+    ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: 'expenses',
+      ...attrs
+    })
+  return [200, {resource: createTag()}]
+}
 
 export const mockTagShow: Mock = config =>{
   const createTag = (attrs?: any) =>
