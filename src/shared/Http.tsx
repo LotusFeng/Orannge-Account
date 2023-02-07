980708337df292db1e27d8aaf5eaef5d8e811599
czf <505487452@qq.com>
2023-02-07 22:20:42 +0800

    初步转换折线图数据

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } f
 import {
   mockItemCreate,
   mockItemIndex,
-  mockItemIndexBalance,
+  mockItemIndexBalance, mockItemSummary,
   mockSession,
   mockTagEdit,
   mockTagIndex,
@@ -63,7 +63,9 @@ const mock = (response: AxiosResponse) => {
     case 'itemIndexBalance':
       [response.status, response.data] = mockItemIndexBalance(response.config)
       return true
-
+    case 'itemSummary':
+      [response.status, response.data] = mockItemSummary(response.config)
+      return true
   }
   return false
 }
